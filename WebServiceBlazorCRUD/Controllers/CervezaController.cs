6bdca5d92156cc2d422a766bebb6345fc0910bc3
Luis Hernández Gómez <ing.luishg76@gmail.com>
2022-02-13 18:12:39 -0500

    Agregar archivos de proyecto.

---
+++ WebServiceBlazorCRUD/Controllers/CervezaController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServiceBlazorCRUD.Models;
+
+namespace WebServiceBlazorCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CervezaController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Cerveza>>> Get() 
+        {
+           using (BlazorCRUDContext db = new BlazorCRUDContext())
+           {
+              return await db.Cervezas.ToListAsync();
+           }
+        }
+
+        [HttpGet("{id}", Name ="detalles_persona")]
+        public async Task<ActionResult<Cerveza>> Get(int id)
+        {
+            using(BlazorCRUDContext db=new BlazorCRUDContext())
+            {
+                return await db.Cervezas.FirstOrDefaultAsync(c => c.Id == id);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Add(Cerveza model)
+        {
+           using (BlazorCRUDContext db = new BlazorCRUDContext())
+           {
+               Cerveza nueva = new Cerveza();
+               nueva.Nombre = model.Nombre;
+               nueva.Marca = model.Marca;
+               db.Cervezas.Add(nueva);
+               db.SaveChangesAsync();
+                return new CreatedAtRouteResult("detalles_persona", new { id = nueva.Id }, nueva);
+           }           
+        }
+
+        [HttpPut]
+        public ActionResult Edit(Cerveza model)
+        {
+           using (BlazorCRUDContext db = new BlazorCRUDContext())
+           {
+              Cerveza modificada = db.Cervezas.FirstOrDefault(c=>c.Id==model.Id);
+              modificada.Nombre = model.Nombre;
+              modificada.Marca = model.Marca;
+              db.Entry(modificada).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+              db.SaveChanges();                    
+           }          
+           return Ok();            
+        }
+
+        [HttpDelete("{Id}")]
+        public ActionResult Delet(int Id)
+        {
+            using (BlazorCRUDContext db = new BlazorCRUDContext())
+            {
+               Cerveza eliminar = db.Cervezas.FirstOrDefault(c => c.Id == Id);
+               db.Cervezas.Remove(eliminar);
+               db.SaveChanges();
+            }
+                     
+            return Ok();
+        }
+    }
+}
---
+++ WebServiceBlazorCRUD/Models/BlazorCRUDContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebServiceBlazorCRUD.Models
+{
+    public partial class BlazorCRUDContext : DbContext
+    {
+        public BlazorCRUDContext()
+        {
+
+        }
+
+        public BlazorCRUDContext(DbContextOptions<BlazorCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cerveza> Cervezas { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+              optionsBuilder.UseSqlServer("Server=pc-luiso;Database=BlazorCRUD; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cerveza>(entity =>
+            {
+                entity.ToTable("cerveza");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Marca)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("marca");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
